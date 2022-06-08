147cb79e5fc79ba8b998815de52758009d8cda2c
lmw2109 <lucas_wagner@me.com>
2022-06-08 13:18:46 +0200

    Added insertion method in bst.py

--- BinarySearchTree/bst.py
+++ BinarySearchTree/bst.py
@@ -3,8 +3,10 @@
 # Blueprint to a single node, sub-class of BST
 class Node:
 
-    def __init__(self, data=None, left=None, right=None, parent=None):
-        # A node carries some sort of data or key
+    def __init__(self, key, data=None, left=None, right=None, parent=None):
+        # A key identifies a node and is needed for insert logic
+        self.key = key
+        # and some sort of data
         self.data = data
         # and a reference to its child_nodes (in this case binary: l/r)
         self.left = left
@@ -27,8 +29,38 @@ class BinarySearchTree:
 
     # Insertion
     def bst_insert(self, new_node):
-        data = new_node.data
+        # key of new_node
+        key = new_node.key
+        # starting node to be investigated
         node = self.root
+        # If there is nodes in the tree:
+        while node is not None:
+            parent = node
+            # Check if key of node is equal to key of new_node
+            if node.key == key:
+                print('Node is already in the tree!')
+            # Check if key of node is larger than key of new_node
+            elif node.key > key:
+                # if so: walk left in the tree
+                node = node.left
+            # Check if key of node is smaller than key of new_node
+            elif node.key < key:
+                # if so: walk right in the tree
+                node = node.right
+        # If there is no nodes in the tree:
+        if parent is None:
+            # Insert node into root
+            self.root = new_node
+        # If key of parent is larger than key of new_node
+        elif parent.key > key:
+            # insert new_node as left child
+            parent.left = new_node
+        # If key of parent is smaller than key of new_node
+        elif parent.key < key:
+            # insert new_node as right child
+            parent.right = new_node
+        # Tell new_node about its parent
+        new_node.parent = parent
 
     # Search
 
